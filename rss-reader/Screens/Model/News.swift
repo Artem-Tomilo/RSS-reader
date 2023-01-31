e8391149e68a55338f43b9460211e47cc44b94cd
Tomilo Artem <artsiom.tamila@gmail.com>
2023-01-31 20:33:26 +0300

    Added News model

---
+++ rss-reader/Screens/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  rss-reader
+//
+//  Created by Артем Томило on 31.01.23.
+//
+
+import UIKit
+
+struct News {
+    var title: String
+    var description: String
+    var date: String
+    var logo: UIImage?
+}
